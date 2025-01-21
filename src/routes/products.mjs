c8433d4fe85715b23dee477d1bf15c77f9745ac3
romainschert <150144520+romainschert@users.noreply.github.com>
2025-01-21 13:24:21 +0100

    fin step 4

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -44,4 +44,22 @@ productsRouter.post("/", (req, res) => {
 
   res.json(success(message, createdProduct));
 });
+
+productsRouter.put("/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = getProduct(productId);
+  // Mise à jour du produit
+  // A noter que la propriété 'created' n'étant pas modifiée, sera conservée telle quelle.
+  const updatedProduct = {
+    id: productId,
+    ...req.body,
+    created: product.created,
+  };
+  updateProduct(productId, updatedProduct);
+  // Définir un message pour l'utilisateur de l'API REST
+  const message = `Le produit ${updatedProduct.name} dont l'id vaut ${productId} a été mis à jour avec succès !`;
+  // Retourner la réponse HTTP en json avec le msg et le produit créé
+  res.json(success(message, updatedProduct));
+});
+
 export { productsRouter };
