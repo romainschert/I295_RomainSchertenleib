2575019c02b5024eed490a85f3ee2c9b838b2b52
romainschert <150144520+romainschert@users.noreply.github.com>
2025-01-28 15:24:45 +0100

    fin de la step 10

--- src/models/products.mjs
+++ src/models/products.mjs
@@ -1,4 +1,11 @@
+import { DataTypes } from "sequelize"; //Importation de l'objet DataTypes depuis le package sequelize.
+// DataTypes est utilisé pour définir les types de données des colonnes de la base de données, comme INTEGER, STRING, etc.
+import { sequelize } from "../db/sequelize.mjs"; //Cela permet d'interagir avec la base de données configurée.
+
 const ProductModel = (sequelize, DataTypes) => {
+  //une fonction est définie pour créer et retourner un modèle Produc
+  // sequelize : L'instance de connexion à la base de données.
+  //DataTypes : L'objet permettant de définir les types des champs.
   return sequelize.define(
     "Product",
     {
@@ -9,18 +16,53 @@ const ProductModel = (sequelize, DataTypes) => {
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: false, // La colonne ne peut accepter une valeur NULL (optionnelle).
+        validate: {
+          // Spécifie des règles de validation pour la valeur.
+          is: {
+            args: /^[A-Za-z]*$/, // Applique une expression régulière (RegEx) pour valider que la valeur contient uniquement des lettres (A-Z, a-z) et des espaces.
+            msg: "Seules les lettres et les espaes sont autorisées.", // : Message d'erreur à afficher si la validation échoue.
+          },
+          notEmpty: {
+            msg: "Le nom ne peut pas être vide.",
+          },
+          notNull: {
+            msg: "Le nom est une propriété obligatoire.",
+          },
+        },
       },
       price: {
         type: DataTypes.FLOAT,
         allowNull: false,
+        validate: {
+          isFloat: {
+            msg: "Utilisez uniquement des nombres pour le prix.",
+          },
+          notEmpty: {
+            msg: "Le prix ne peut pas être vide.",
+          },
+          notNull: {
+            msg: "Le prix est une propriété obligatoire.",
+          },
+          min: {
+            args: [1.0],
+            msg: "Le prix doit être supérieur à 1$.",
+          },
+          max: {
+            args: [1000.0],
+            msg: "Le prix doit être inférieur à 1000$.",
+          },
+        },
       },
     },
     {
-      timestamps: true,
-      createdAt: "created",
-      updatedAt: false,
+      timestamps: true, //Active automatiquement les colonnes de suivi des dates :
+      // createdAt : Date de création.
+      // updatedAt : Date de la dernière mise à jour.
+      createdAt: "created", //Renomme la colonne createdAt en created dans la table.
+      updatedAt: false, //Désactive la colonne updatedAt.
     }
   );
 };
-export { ProductModel };
+
+export { ProductModel }; //Permet d'exporter la fonction ProductModel pour qu'elle soit utilisée ailleurs dans le projet.
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -27,17 +27,30 @@ productsRouter.get("/:id", (req, res) => {
     });
 });
 productsRouter.post("/", (req, res) => {
-  Product.create(req.body)
+  // Déclare une route POST à l'URL racine (/) du routeur productsRouter.
+  // Utilisée pour ajouter un produit dans la base de données.
+
+  Product.create(req.body) // Utilise la méthode create de Sequelize pour insérer un nouveau produit dans la base de données. Les données du produit sont extraites de req.body (un JSON envoyé par le client).
     .then((createdProduct) => {
+      // Si la création réussit, le produit créé est passé à la fonction dans le bloc then
       // Définir un message pour le consommateur de l'API REST
       const message = `Le produit ${createdProduct.name} a bien été créé !`;
       // Retourner la réponse HTTP en json avec le msg et le produit créé
-      res.json(success(message, createdProduct));
+      res.json(success(message, createdProduct)); // Retourne une réponse HTTP au format JSON.
     })
     .catch((error) => {
+      // Si une erreur se produit lors de l'exécution de Product.create, le bloc catch est exécuté.
+      if (error instanceof ValidationError) {
+        // Vérifie si l'erreur est de type ValidationError (propre à Sequelize).
+        // Cela arrive lorsque les données envoyées par le client ne respectent pas les contraintes définies dans le modèle.
+        return res.status(400).json({ message: error.message, data: error }); // Envoie une réponse avec le code HTTP 400 (mauvaise requête).
+        // Retourne un objet JSON contenant :message : Description de l'erreur.data : Détails techniques de l'erreur.
+      }
       const message =
         "Le produit n'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
-      res.status(500).json({ message, data: error });
+      res.status(500).json({ message, data: error }); // Si l'erreur n'est pas une ValidationError, un message générique est retourné.
+      // res.status(500) : Code HTTP 500 indique une erreur interne au serveur.
+      // Le message et les détails de l'erreur sont inclus dans la réponse JSON.
     });
 });
 productsRouter.delete("/:id", (req, res) => {
